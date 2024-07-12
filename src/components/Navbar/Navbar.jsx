4773dbbff792d50258e2a0435f2c854c833f9143
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-07-11 18:21:53 -0700

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.scss'
+
+const Navbar = () => {
+
+
+
+    return (
+        <nav className="nav">
+            <div>Katherine Phan</div>
+            <div>
+                <ul className="nav__link">
+                    <li>About</li>
+                    <li>Experience</li>
+                    <li>Project</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Navbar
