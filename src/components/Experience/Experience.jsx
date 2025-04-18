f955f743a910653f0fd94c09941173b75a13580b
Katherine Phan <katherinephan21@gmail.com>
2025-04-17 17:06:52 -0700

    Added professional section in Experience comp

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,14 +1,19 @@
 import './Experience.scss'
 import BrainStationLogo from '../../assets/images/brainstation.png'
 import SprottShawLogo from '../../assets/images/SprottShaw.gif'
+import CherryOnTechLogo from '../../assets/images/CherryOnTech.png'
+import EndeavorTrustLogo from '../../assets/images/EndeavorTrust.png'
+import TheCareGroupLogo from '../../assets/images/TCC.png'
+import css from '../../assets/skills/CSS.svg'
+
 const Experience = () => {
 
     return (
         <section className="experience">
             <p className="experience__text-p1">Explore My</p>
             <h1 className="experience__title">Experience</h1>
-            <section className='experience__education'>
-                <h2>Education</h2>
+            <section className='experience__subheader'>
+                <h2>EDUCATION</h2>
                 <div className='experience__container'>
                     <img src={BrainStationLogo} alt="BrainStation Logo" className="experience__logo"/>
                     <ul className='experience__list'>
@@ -18,13 +23,46 @@ const Experience = () => {
                     </ul>
                 </div>
                 <div className='experience__container'>
-                    <img src={SprottShawLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--padding"/>
+                    <img src={SprottShawLogo} alt="Sprott Shaw College Logo" className="experience__logo"/>
                     <ul className='experience__list'>
                         <li className='experience__item'>Sprott Shaw College</li>
                         <li className='experience__item'>Sep 2014 - Jun 2016</li>
                         <li className='experience__item'>Diploma, License Practical Nursing</li>
                     </ul>
                 </div>
+            </section>
+            <section className='experience__subheader'>
+                <h2>PROFESSIONAL</h2>
+                <div className='experience__container'>
+                    <img src={CherryOnTechLogo} alt="BrainStation Logo" className="experience__logo"/>
+                    <ul className='experience__list'>
+                        <li className='experience__item'>CherryOnTech</li>
+                        <li className='experience__item'>Jul 2024 - Oct 2024</li>
+                        <li className='experience__item'>Web Developer</li>
+                    </ul>
+                </div>
+                <div className='experience__container'>
+                    <img src={EndeavorTrustLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--smaller"/>
+                    <ul className='experience__list'>
+                        <li className='experience__item'>Endeavor Trust</li>
+                        <li className='experience__item'>Mar 2022 - Dec 2023</li>
+                        <li className='experience__item'>Transfer Agent</li>
+                    </ul>
+                </div>
+                <div className='experience__container'>
+                    <img src={TheCareGroupLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--padding"/>
+                    <ul className='experience__list'>
+                        <li className='experience__item'>Point Grey Private Hospital</li>
+                        <li className='experience__item'>Dec 2021 - Feb 2022</li>
+                        <li className='experience__item'>License Practical Nursing</li>
+                    </ul>
+                </div>
+            </section>
+            <section>
+                <h2>SKILLS</h2>
+                <select>
+                    <option><img src={css}/></option>
+                </select>
             </section>
 
 
