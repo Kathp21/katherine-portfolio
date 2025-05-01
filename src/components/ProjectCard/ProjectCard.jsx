f10782f1d08e8a45e97728a260fde96aea0acc81
Katherine Phan <katherinephan21@gmail.com>
2025-05-01 16:59:01 -0700

    Added horizontal scroll for badges list

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -17,6 +17,16 @@ const ProjectCard = ({projects}) => {
                         <h2 className="project-card__summary-title">{project.name}</h2>
                         <div className="project-card__details">
                             <p className="project-card__summary-text">{project.summary}</p>
+                            <div className="project-card__tools-scroll-wrapper">
+                                <h3 className="project-card__subheader">Built With:</h3>
+                                <div className="project-card__tools-list">
+                                    <div className="project-card__tools-strip">
+                                    {[...project.badge, ...project.badge].map((src, i) => (
+                                        <img key={i} src={src} alt={`badge-${i}`} className="project-card__badge" />
+                                    ))}
+                                    </div>
+                                </div>
+                            </div>
                             <div className="project-card__button-container">
                                 <button className="project-card__button">GitHub</button>
                                 <button className="project-card__button">Live Demo</button>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,13 +6,57 @@ import instock from '../../assets/images/instock-2.png'
 import tripCrafters from '../../assets/images/tripCrafters-1.png'
 import empowerMe from '../../assets/images/empowerMe-2.png'
 import portfolio from '../../assets/images/portfolio-1.png'
+import sassBadge from '../../assets/badges/sass-badge.svg'
+// import html5Badge from '../../assets/badges/html-badge.svg'
+// import canvasBadge from '../../assets/badges/canvas-badges.svg'
+import viteBadge from '../../assets/badges/vite-badge.svg'
+import tailwindBadge from '../../assets/badges/tailwind-badge.svg'
+import reactBadge from '../../assets/badges/react-banner.svg'
+import nodeBadge from '../../assets/badges/node-badge.svg'
+import netlifyBadge from '../../assets/badges/netlify-badge.svg'
+import vercelBadge from '../../assets/badges/vercel-badge.svg'
+import mysqlBadge from '../../assets/badges/mysql-banner.svg'
+import jiraBadge from '../../assets/badges/jira-badge.svg'
+import javascriptBadge from '../../assets/badges/javascript-badge.svg'
+import githubBadge from '../../assets/badges/github-badge.svg'
+import expressBadge from '../../assets/badges/express-badge.svg'
+// import herokuBadge from '../../assets/badges/heroku-badge.svg'
+import postmanBadge from '../../assets/badges/postman-badge.svg'
+import jsonBadge from '../../assets/badges/json-badge.png'
+import jsonTokenBadge from '../../assets/badges/JWT-black-badge.svg'
+import axiosBadge from '../../assets/badges/axios-badge.png'
+import chatGPTBadge from '../../assets/badges/chatGPT-badge.svg'
+import typescriptBadge from '../../assets/badges/typescript-badge.svg'
+import figmaBadge from '../../assets/badges/figma-badge.svg'
 
 const projectsInfo = [
-    { name: "Trip Crafters", image: tripCrafters, summary: "Plan personalized trips with smart recommendations."},
-    { name: "EmpowerMe", image: empowerMe },
-    { name: "Portfolio", image: portfolio, summary: "My personal portfolio, showcasing my background, skills, and the projects I've developed." },
-    { name: "Instock", image: instock, summary: "InStock is a fully responsive full-stack web app providing an inventory management system for a Fortune 500 client. It displays a list of warehouse inventory." },
-    { name: "Brain Flix", image: brainFlix, summary: "Brain Flix is a fully responsive video streaming platform." },
+    { 
+        name: "Trip Crafters", image: tripCrafters, 
+        summary: "Plan personalized trips with smart recommendations.", 
+        badge: [reactBadge, chatGPTBadge, javascriptBadge, sassBadge, jsonTokenBadge, axiosBadge, postmanBadge, netlifyBadge, expressBadge, nodeBadge,githubBadge, vercelBadge]},
+    { 
+        name: "EmpowerMe", 
+        image: empowerMe,
+        badge: [sassBadge, reactBadge, viteBadge, typescriptBadge, jiraBadge, figmaBadge, tailwindBadge,githubBadge]
+    },
+    { 
+        name: "Portfolio", 
+        image: portfolio, 
+        summary: "My personal portfolio, showcasing my background, skills, and the projects I've developed.",  
+        badge: [sassBadge, reactBadge, javascriptBadge, netlifyBadge, githubBadge] 
+    },
+    { 
+        name: "Instock", 
+        image: instock, 
+        summary: "InStock is a fully responsive full-stack web app providing an inventory management system for a Fortune 500 client. It displays a list of warehouse inventory.",
+        badge: [sassBadge, reactBadge, javascriptBadge, expressBadge, githubBadge, nodeBadge, axiosBadge, jsonBadge, postmanBadge, mysqlBadge]
+    },
+    { 
+        name: "Brain Flix", 
+        image: brainFlix, 
+        summary: "Brain Flix is a fully responsive video streaming platform.",
+        badge: [sassBadge, reactBadge, javascriptBadge, expressBadge, githubBadge, nodeBadge, axiosBadge, jsonBadge, postmanBadge, mysqlBadge]
+    },
 ]
 
 const Projects = () => {
