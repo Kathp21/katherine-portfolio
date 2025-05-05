cc68da5f29541b13ce6768a457e0df65a61ddd95
Katherine Phan <katherinephan21@gmail.com>
2025-05-05 16:03:07 -0700

    Change style of experience section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -14,52 +14,55 @@ const Experience = () => {
         <section className="experience">
             <p className="experience__text-p1">Explore My</p>
             <h1 className="experience__title">Experience</h1>
-            <section className='experience__education'>
-                <h2  className='experience__subheader'>EDUCATION</h2>
-                <div className='experience__container'>
-                    <img src={BrainStationLogo} alt="BrainStation Logo" className="experience__logo"/>
-                    <ul className='experience__list'>
-                        <li className='experience__item'>BrainStation</li>
-                        <li className='experience__item'>Dec 2023 - Mar 2024</li>
-                        <li className='experience__item'>Diploma, Software Engineering</li>
-                    </ul>
-                </div>
-                <div className='experience__container'>
-                    <img src={SprottShawLogo} alt="Sprott Shaw College Logo" className="experience__logo"/>
-                    <ul className='experience__list'>
-                        <li className='experience__item'>Sprott Shaw College</li>
-                        <li className='experience__item'>Sep 2014 - Jun 2016</li>
-                        <li className='experience__item'>Diploma, License Practical Nursing</li>
-                    </ul>
-                </div>
-            </section>
-            <section className='experience__education'>
-                <h2 className='experience__subheader'>PROFESSIONAL</h2>
-                <div className='experience__container'>
-                    <img src={CherryOnTechLogo} alt="BrainStation Logo" className="experience__logo"/>
-                    <ul className='experience__list'>
-                        <li className='experience__item'>CherryOnTech</li>
-                        <li className='experience__item'>Jul 2024 - Oct 2024</li>
-                        <li className='experience__item'>Web Developer</li>
-                    </ul>
-                </div>
-                <div className='experience__container'>
-                    <img src={EndeavorTrustLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--smaller"/>
-                    <ul className='experience__list'>
-                        <li className='experience__item'>Endeavor Trust</li>
-                        <li className='experience__item'>Mar 2022 - Dec 2023</li>
-                        <li className='experience__item'>Transfer Agent</li>
-                    </ul>
-                </div>
-                <div className='experience__container'>
-                    <img src={TheCareGroupLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--padding"/>
-                    <ul className='experience__list'>
-                        <li className='experience__item'>Point Grey Private Hospital</li>
-                        <li className='experience__item'>Dec 2021 - Feb 2022</li>
-                        <li className='experience__item'>License Practical Nursing</li>
-                    </ul>
-                </div>
-            </section>
+            <div className='experience__section-container'>
+                <section className='experience__section-education'>
+                    <h2  className='experience__subheader'>EDUCATION</h2>
+                    <div className='experience__container'>
+                        <img src={BrainStationLogo} alt="BrainStation Logo" className="experience__logo"/>
+                        <ul className='experience__list'>
+                            <li className='experience__item'>BrainStation</li>
+                            <li className='experience__item'>Dec 2023 - Mar 2024</li>
+                            <li className='experience__item'>Diploma, Software Engineering</li>
+                        </ul>
+                    </div>
+                    <div className='experience__container'>
+                        <img src={SprottShawLogo} alt="Sprott Shaw College Logo" className="experience__logo"/>
+                        <ul className='experience__list'>
+                            <li className='experience__item'>Sprott Shaw College</li>
+                            <li className='experience__item'>Sep 2014 - Jun 2016</li>
+                            <li className='experience__item'>Diploma, License Practical Nursing</li>
+                        </ul>
+                    </div>
+                </section>
+                <section className='experience__section-professional'>
+                    <h2 className='experience__subheader'>PROFESSIONAL</h2>
+                    <div className='experience__container'>
+                        <img src={CherryOnTechLogo} alt="BrainStation Logo" className="experience__logo"/>
+                        <ul className='experience__list'>
+                            <li className='experience__item'>CherryOnTech</li>
+                            <li className='experience__item'>Jul 2024 - Oct 2024</li>
+                            <li className='experience__item'>Web Developer</li>
+                        </ul>
+                    </div>
+                    <div className='experience__container'>
+                        <img src={EndeavorTrustLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--smaller"/>
+                        <ul className='experience__list'>
+                            <li className='experience__item'>Endeavor Trust</li>
+                            <li className='experience__item'>Mar 2022 - Dec 2023</li>
+                            <li className='experience__item'>Transfer Agent</li>
+                        </ul>
+                    </div>
+                    <div className='experience__container'>
+                        <img src={TheCareGroupLogo} alt="Sprott Shaw College Logo" className="experience__logo experience__logo--padding"/>
+                        <ul className='experience__list'>
+                            <li className='experience__item'>Point Grey Private Hospital</li>
+                            <li className='experience__item'>Dec 2021 - Feb 2022</li>
+                            <li className='experience__item'>License Practical Nursing</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+
             <section className='experience__skills-section'>
                 <h2>SKILLS</h2>
                 <DropdownTechnical/>
