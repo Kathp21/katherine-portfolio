9031b0ecc5d66b85c0c846048bcdf89a26364949
Katherine Phan <katherinephan21@gmail.com>
2025-05-06 16:12:25 -0700

    Added chat box

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import './Contact.scss'
-import mail from '../../assets/icons/email.png'
-import linkedin from '../../assets/icons/linkedin.png'
+// import mail from '../../assets/icons/email.png'
+// import linkedin from '../../assets/icons/linkedin.png'
+import { FaUser } from "react-icons/fa";
+
 
 const Contact = () => {
 
@@ -8,7 +10,7 @@ const Contact = () => {
         <section className='contact'>
             <p className='contact__text-p1'>Get in Touch</p>
             <h1 className='contact__title'>Contact Me</h1>
-            <div className='contact__outer-container'>
+            {/* <div className='contact__outer-container'>
                 <div className='contact__info-box'>
                     <div className='contact__info-container'>
                         <img
@@ -27,7 +29,48 @@ const Contact = () => {
                         <p className='contact__text'><a href='https://www.linkedin.com/in/katherine-phan21' target='_blank' rel='noopener noreferrer'>Linkedin</a></p>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <section className='contact__container'>
+                <div className='contact__screen'>
+                    <div className='contact__screen-header'>
+                        <div className='contact__screen-header contact__screen-header--left'>
+                            <div className='contact__header-button contact__header-button--close'></div>
+                            <div className='contact__header-button contact__header-button--maximize'></div>
+                            <div className='contact__header-button contact__header-button--minimize'></div>
+                        </div>
+                        <div className='contact__screen-header contact__screen-header--right'>
+                            <div className='contact__header-button contact__header-button--ellipsis'></div>
+                            <div className='contact__header-button contact__header-button--ellipsis'></div>
+                            <div className='contact__header-button contact__header-button--ellipsis'></div>
+                        </div>
+                    </div>
+                    <div className='contact__screen-content'>
+                        <div className='contact__screen-item'>
+                            <div className='contact__screen-title'>
+                                <h2 className='contact__header'>Let's connect!</h2>
+                            </div>
+                            <p className='contact__screen-text'>If you'd like to connect or have any questions, ideas, or feedback, please fill out the form. I'll get back to you soon!</p>
+                        </div>
+                        <form className='contact__screen-form'>
+                            <div className='contact__input-section'>
+                                <div className='contact__form-label'>
+                                    <label>NAME</label>
+                                    <FaUser />
+                                </div>
+
+                                <input
+                                    type='text'
+                                    className='contact__input-name'
+                                />
+                                
+                            </div>
+
+
+                        </form>
+                    </div>
+
+                </div>
+            </section>
         </section>
     )
 }
