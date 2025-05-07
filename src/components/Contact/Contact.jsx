df8594d348e1f0ad6b6c9b6beca31bb9a27b54dd
Katherine Phan <katherinephan21@gmail.com>
2025-05-07 16:14:04 -0700

    Added formspree for form submission

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,36 +3,31 @@ import './Contact.scss'
 // import linkedin from '../../assets/icons/linkedin.png'
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-
+import {useForm, ValidationError} from '@formspree/react';
+import { useState, useEffect } from 'react';
 
 
 
 const Contact = () => {
+    const [state, handleSubmit] = useForm("myzwjjwl")
+    const [showSuccess, setShowSuccess] = useState(false)
+
+    useEffect(() => {
+        if(state.succeeded){
+            setShowSuccess(true)
+
+            const timer = setTimeout(() => {
+                setShowSuccess(false)
+            }, 3000)
+
+            return () => clearTimeout(timer)
+        }
+    }, [state.succeeded])
 
     return (
         <section className='contact'>
             <p className='contact__text-p1'>Get in Touch</p>
             <h1 className='contact__title'>Contact Me</h1>
-            {/* <div className='contact__outer-container'>
-                <div className='contact__info-box'>
-                    <div className='contact__info-container'>
-                        <img
-                            src={mail}
-                            alt='Email icon'
-                            className='contact__icon contact__icon--email'
-                        />
-                        <p className='contact__text'><a href='mailto:katherinephan21@gmail.com' target='_blank' rel='noopener noreferrer'>Katherinephan21@gmail.com</a></p>
-                    </div>
-                    <div className='contact__info-container'>
-                        <img
-                            src={linkedin}
-                            alt='Linkedin icon'
-                            className='contact__icon'
-                        />
-                        <p className='contact__text'><a href='https://www.linkedin.com/in/katherine-phan21' target='_blank' rel='noopener noreferrer'>Linkedin</a></p>
-                    </div>
-                </div>
-            </div> */}
             <section className='contact__container'>
                 <div className='contact__screen'>
                     <div className='contact__screen-header'>
@@ -47,57 +42,73 @@ const Contact = () => {
                             <div className='contact__header-button contact__header-button--ellipsis'></div>
                         </div>
                     </div>
+
                     <div className='contact__screen-content'>
-                        <div className='contact__text-form-container'>
-                            <div className='contact__screen-item'>
-                                <div className='contact__screen-title'>
-                                    <h2 className='contact__header'>Let's talk!</h2>
+                        {showSuccess ? (
+                            <p className='contact__success-message'>Thansk for connecting!</p>
+                        ) : (
+                            <div className='contact__text-form-container'>
+                                <div className='contact__screen-item'>
+                                    <div className='contact__screen-title'>
+                                        <h2 className='contact__header'>Let's talk!</h2>
+                                    </div>
+                                    <p className='contact__screen-text'>If you'd like to connect or have any questions, ideas, or feedback, please fill out the form. I'll get back to you soon!</p>
                                 </div>
-                                <p className='contact__screen-text'>If you'd like to connect or have any questions, ideas, or feedback, please fill out the form. I'll get back to you soon!</p>
+                                <form 
+                                    className='contact__screen-form'
+                                    onSubmit={handleSubmit}
+                                >
+                                    <div className='contact__input-section'>
+                                        <div className='contact__form-label'>
+                                            <label className='contact__label'>NAME</label>
+                                            <FaUser className='contact__icon' />
+                                        </div>
+
+                                        <input
+                                            type='text'
+                                            name='name'
+                                            className='contact__input-info'
+                                            required
+                                        />
+                                    </div>
+                                    <div className='contact__input-section'>
+                                        <div className='contact__form-label'>
+                                            <label className='contact__label'>EMAIL</label>
+                                            <MdEmail className='contact__icon' />
+                                        </div>
+
+                                        <input
+                                            type='email'
+                                            name='email'
+                                            className='contact__input-info'
+                                            required
+                                        />
+                                        <ValidationError
+                                            prefix='Email'
+                                            field='email'
+                                            errors={state.errors}
+                                        />
+                                    </div>
+                                    <div className='contact__input-section'>
+                                        <div className='contact__form-label contact__form-label--message'>
+                                            <label className='contact__label'>MESSAGE</label>
+                                        </div>
+
+                                        <textarea
+                                            type='text'
+                                            name='message'
+                                            className='contact__input-message'
+                                            rows={5}
+                                        />
+                                    </div>
+
+                                    <div className='contact__btn-container'>
+                                        <button className='contact__send-btn' type='submit' disabled={state.submitting}>Send</button>
+                                    </div>
+                                </form>
                             </div>
-                            <form className='contact__screen-form'>
-                                <div className='contact__input-section'>
-                                    <div className='contact__form-label'>
-                                        <label className='contact__label'>NAME</label>
-                                        <FaUser className='contact__icon' />
-                                    </div>
-
-                                    <input
-                                        type='text'
-                                        className='contact__input-info'
-                                    />
-                                </div>
-                                <div className='contact__input-section'>
-                                    <div className='contact__form-label'>
-                                        <label className='contact__label'>EMAIL</label>
-                                        <MdEmail className='contact__icon' />
-                                    </div>
-
-                                    <input
-                                        type='email'
-                                        className='contact__input-info'
-                                    />
-                                </div>
-                                <div className='contact__input-section'>
-                                    <div className='contact__form-label contact__form-label--message'>
-                                        <label className='contact__label'>MESSAGE</label>
-                                    </div>
-
-                                    <textarea
-                                        type='text'
-                                        className='contact__input-message'
-                                        rows={5}
-                                    />
-                                </div>
-                            </form>
-                        </div>
-
-
-                        <div className='contact__btn-container'>
-                            <button className='contact__send-btn'>Send</button>
-                        </div>
+                        )}
                     </div>
-
                 </div>
             </section>
         </section>
